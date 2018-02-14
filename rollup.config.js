cc739a644d168ac234c3fde8a91a50448eac68d0
Ian Johnson <ianprime0509@gmail.com>
2018-02-14 09:23:30 -0500

    Add Babel to transpile to ES5

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,18 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'lib/index.js',
     format: 'cjs',
   },
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: [['env', { modules: false }]],
+      plugins: ['external-helpers'],
+      exclude: 'node_modules/**',
+    }),
+  ],
   external: ['fft.js', 'next-pow-2'],
 };
