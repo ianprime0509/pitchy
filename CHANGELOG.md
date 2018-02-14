db86ca1651ff7deefdda84621183fb2dc9e2918c
Ian Johnson <ianprime0509@gmail.com>
2018-02-14 09:38:45 -0500

    Transpile ES module output to ES5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic
 Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+- Transpile ES module output to ES5 as well (`index.mjs`).
 
 ## [1.0.1] - 2018-02-14
 ### Changed
--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,16 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'lib/index.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: 'lib/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'lib/index.mjs',
+      format: 'es',
+    },
+  ],
   plugins: [
     babel({
       babelrc: false,
