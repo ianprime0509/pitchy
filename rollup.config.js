ecbcfa690a20ea950c7c895ba3a35f918ca169df
Ian Johnson <ianprime0509@gmail.com>
2021-01-02 10:18:55 -0500

    Add UMD bundle to output
    This is for compatibility with unpkg.com.

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,44 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import babel from "rollup-plugin-babel";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "lib/index.js",
-      format: "cjs",
-    },
-    {
-      file: "lib/index.mjs",
-      format: "es",
-    },
-  ],
-  plugins: [
-    babel({
-      exclude: "node_modules/**",
-      extensions: [".ts", ".js"],
-    }),
-  ],
-  external: ["fft.js", "next-pow-2"],
-};
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "lib/index.js",
+        format: "cjs",
+      },
+      {
+        file: "lib/index.mjs",
+        format: "es",
+      },
+    ],
+    plugins: [
+      babel({
+        exclude: "node_modules/**",
+        extensions: [".ts", ".js"],
+      }),
+    ],
+    external: ["fft.js", "next-pow-2"],
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "umd/index.js",
+        format: "umd",
+        name: "pitchy",
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      babel({
+        exclude: "node_modules/**",
+        extensions: [".ts", ".js"],
+      }),
+    ],
+  },
+];
